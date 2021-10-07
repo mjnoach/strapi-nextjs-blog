dda56f01216b9cd87064c55d746daf485165c65a
mjnoach <a.siennkiewicz@gmail.com>
2021-10-07 20:13:17 +0200

    refactor(frontend): move useClientRect to hooks directory

--- frontend/hooks/useClientRect.js
+++ frontend/hooks/useClientRect.js
@@ -2,9 +2,8 @@ import { useState, useCallback } from "react"
 
 /**
  * Returns an array containing:
- *  domRect - a DOMRect object describing the size and the position of a rectangle on a viewport
+ *  domRect - a DOMRect object describing the size and position of a rectangle
  *  domRectRef - a memoized callback for keeping references to nodes' DOMRect objects
- * @returns {Array}
  */
 export const useClientRect = () => {
   const [domRect, setRect] = useState(null)
